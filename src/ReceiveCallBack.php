81fe9b448e5921b78e54afd0d10c043d122e16dd
bagher sohrabi <bgsrb88@gmail.com>
2017-08-03 10:13:18 +0430

    Create ReceiveCallBack.php

---
+++ src/ReceiveCallBack.php
@@ -0,0 +1,9 @@
+<?php 
+$receive=array(
+  "from"=>$_REQUEST["from"],
+  "to"=>$_REQUEST["to"],
+  "message"=>$_REQUEST["message"],
+  "messageid"=>$_REQUEST["messageid"],
+);
+var_dump($receive);
+?>
